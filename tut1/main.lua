ade49d49ccd6373358ecbde0d7d0590b64382415
Gage Pierce <piercegage@gmail.com>
2015-04-19 08:21:25 -0400

    Clean comments

--- tut1/main.lua
+++ tut1/main.lua
@@ -4,7 +4,6 @@ require "enemy"
 
 
 function love.load()
-  -- Changed getBackgroundColor to setBackgroundColor. Now this will actually work.
 	love.graphics.setBackgroundColor(255,255,255)
 	background = love.graphics.newImage('background.png')
 	missile.load()
@@ -12,7 +11,7 @@ function love.load()
 end
 
 function love.mousepressed(x, y, button)
-  -- Changed "1" to "l". Proper mouse buttons are as follow: "l", "m", "r", "wu", "wd"
+
 	if button == "l" then
 		local strX = player.x + player.width / 2
 		local strY = player.y - 3
@@ -21,14 +20,10 @@ function love.mousepressed(x, y, button)
 
 		local angle = math.atan2((mouseY - strY), (mouseX - strX))
     
-    -- Changed local missileDx and local missileDy to missile.dx and missile.dy.
-    -- Change missileSpeed to missile.speed to actually work with what you wrote elsewhere.
+
 		missile.dx = missile.speed * math.cos(angle)
 		missile.dy = missile.speed * math.sin(angle)
 
-    -- Changed missile to missiles
-    -- Changed missileDx, missileDy, and missileImg to missile.dx, missile.dy, and missile.img
-    -- It's important to keep your actual missiles in a different table from your missile variables.
 		table.insert(missiles, {x = strX, y = strY, dx = missile.dx, dy = missile.dy, img = missile.img})
 	end
 end
